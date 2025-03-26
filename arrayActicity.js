89ce3d7747e2a2db7c3a99c9ec426678a584f4ae
mitzie-ponce <mitzieponce0@gmail.com>
2025-03-26 12:55:09 +0800

    Create arrayActicity.js

---
+++ arrayActicity.js
@@ -0,0 +1,9 @@
+const favSingers = ["Adele", "Ed Sheeran", "Taylor Swift"];
+console.log(favSingers[0]);
+
+const mixedArr = ["string", [1,2,3], 123, true];
+console.log(mixedArr [0]);
+console.log(mixedArr [1]);
+console.log(mixedArr [2]);
+console.log(mixedArr [3]);
+
